637b86e0eeee6db54d5644e203cdd13538b657d2
Ning <ning.wei@int-trade.de>
2018-11-18 11:39:12 +0100

    using streamhandler to log into a file

--- dino_container/roar.php
+++ dino_container/roar.php
@@ -2,13 +2,18 @@
 
 namespace Dino\Play;
 
+use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 require  __DIR__ . '/../vendor/autoload.php';
 
 $container = new ContainerBuilder();
-$logger = new Logger('main');
+
+$streamhandler = new StreamHandler(__DIR__.'/dino.log');
+$container->set('logger.stream_handler', $streamhandler);
+
+$logger = new Logger( 'main', array($container->get('logger.stream_handler')) );
 $container->set('logger', $logger);
 
 runApp($container);
